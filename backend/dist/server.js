9be778d57b21f3d58265b3991cb5eefd2849ea1c
LAPTOP WORLD <krishnabarua2010gmail.com>
2026-05-05 02:32:53 +0530

    fix: implement env loading at entry and debug start script

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -36,6 +36,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const fastify_1 = __importDefault(require("fastify"));
 const cors_1 = __importDefault(require("@fastify/cors"));
 const path_1 = __importDefault(require("path"));
